4e030ef46684869459cc948e01c3d118a209dda8
s-nakajima <nakajimashouhei@gmail.com>
2015-07-24 07:11:43 +0900

    Fix by change specification of plugin manager

---
+++ Config/Migration/001_plugin_records.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Add plugin migration
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsMigration', 'NetCommons.Config/Migration');
+
+/**
+ * Add plugin migration
+ *
+ * @package NetCommons\PluginManager\Config\Migration
+ */
+class PluginRecords extends NetCommonsMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'plugin_records';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(),
+		'down' => array(),
+	);
+
+/**
+ * plugin data
+ *
+ * @var array $migration
+ */
+	public $records = array(
+		'Plugin' => array(
+			'key' => 'access_counters',
+			'namespace' => 'netcommons/access-counters',
+			'name' => 'Access counters',
+			'type' => 1,
+			'default_action' => 'access_counters/view',
+			'default_setting_action' => 'access_counter_blocks/index',
+		),
+		'PluginsRole' => array(
+			array('role_key' => 'room_administrator'),
+		),
+		'PluginsRoom' => array(
+			array('room_id' => '1'),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		$this->loadModels([
+			'Plugin' => 'PluginManager.Plugin',
+		]);
+
+		if ($direction === 'down') {
+			$this->Plugin->deletePlugin($this->records);
+		} else {
+			$this->Plugin->savePlugin($this->records);
+		}
+		return true;
+	}
+}
