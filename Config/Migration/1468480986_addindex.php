5a9b151ee7b32a9d53262b028cfa9a1605b6f2a8
kteraguchi <kteraguchi@commonsnet.org>
2016-07-14 17:55:22 +0900

    Add index

---
+++ Config/Migration/1468480986_addindex.php
@@ -0,0 +1,74 @@
+<?php
+class AddIndex extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'AddIndex';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'alter_field' => array(
+				'access_counter_frame_settings' => array(
+					'frame_key' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'index', 'collate' => 'utf8_general_ci', 'comment' => 'frame key | フレームKey | frames.key | ', 'charset' => 'utf8'),
+				),
+				'access_counters' => array(
+					'block_key' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'index', 'collate' => 'utf8_general_ci', 'comment' => 'block key | ブロックKey | blocks.key | ', 'charset' => 'utf8'),
+				),
+			),
+			'create_field' => array(
+				'access_counter_frame_settings' => array(
+					'indexes' => array(
+						'frame_key' => array('column' => 'frame_key', 'unique' => 0),
+					),
+				),
+				'access_counters' => array(
+					'indexes' => array(
+						'block_key' => array('column' => 'block_key', 'unique' => 0),
+					),
+				),
+			),
+		),
+		'down' => array(
+			'alter_field' => array(
+				'access_counter_frame_settings' => array(
+					'frame_key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'frame key | フレームKey | frames.key | ', 'charset' => 'utf8'),
+				),
+				'access_counters' => array(
+					'block_key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'block key | ブロックKey | blocks.key | ', 'charset' => 'utf8'),
+				),
+			),
+			'drop_field' => array(
+				'access_counter_frame_settings' => array('indexes' => array('frame_key')),
+				'access_counters' => array('indexes' => array('block_key')),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
