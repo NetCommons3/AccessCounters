0c11daa500abd10ad1558dcc452565a7a7a5857c
s-nakajima <nakajimashouhei@gmail.com>
2016-10-29 12:32:35 +0900

    ボックスの切り替え機能追加による修正 https://github.com/NetCommons3/NetCommons3/issues/726

--- Config/Migration/001_plugin_records.php
+++ Config/Migration/001_plugin_records.php
@@ -70,11 +70,11 @@ class PluginRecords extends NetCommonsMigration {
 		),
 		'PluginsRoom' => array(
 			//パブリックスペース
-			array('room_id' => '1', 'plugin_key' => 'access_counters', ),
-			//プライベートスペース
 			array('room_id' => '2', 'plugin_key' => 'access_counters', ),
-			//グループスペース
+			//プライベートスペース
 			array('room_id' => '3', 'plugin_key' => 'access_counters', ),
+			//グループスペース
+			array('room_id' => '4', 'plugin_key' => 'access_counters', ),
 		),
 	);
 
